d4529dffe8a67c41a55c4f75b6e677cf0809aaf8
szczuras7 <szczuras7@proton.me>
2025-01-09 19:29:27 +0100

    Początek nawigacji

---
+++ Interface_prototype/Pages/ChooseUserPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Interface_prototype.Pages
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ChooseUserPage.xaml
+    /// </summary>
+    public partial class ChooseUserPage : Page
+    {
+        public ChooseUserPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnUserLog_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow? mainWindow = Window.GetWindow(this) as MainWindow;
+            mainWindow.MainFrame.Navigate(new Uri("Pages/UserLogPage.xaml", UriKind.Relative));
+        }
+    }
+}
--- Interface_prototype/Pages/UserLogPage.xaml.cs
+++ Interface_prototype/Pages/UserLogPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,18 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Interface_prototype
+namespace Interface_prototype.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logika interakcji dla klasy UserLogPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class UserLogPage : Page
     {
-        public MainWindow()
+        public UserLogPage()
         {
             InitializeComponent();
-            MainFrame.Navigate(new Uri("Pages/ChooseUserPage.xaml", UriKind.Relative));
         }
-
     }
 }
